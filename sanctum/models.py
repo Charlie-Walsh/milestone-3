d7458119d2e3c471c47d782cdb4752a56d891a9b
Charlie-Walsh <charlie.walsh.141@gmail.com>
2022-08-10 21:39:52 +0000

    base template and routes added. db models in progress. static files added

---
+++ sanctum/models.py
@@ -0,0 +1,33 @@
+from sanctum import db
+
+
+class Title(db.Model):
+    # schema for Title model
+    id = db.Column(db.Integer, primary_key=True)
+    book_title = db.Column(db.String(50), unique=True, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey(), nullable=False)
+    average_rating = db.Column(db.Integer, nullable = False)
+
+    def __repr__(self):
+        return self.book_title
+
+class Author(db.Model):
+    # schema for Author model
+    id = db.Column(db.Integer, primary_key=True)
+    author_fname = db.Column(db.String(20), nullable=False)
+    author_lname = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return self
+
+
+class Review(db.Model):
+    # schema for Review model
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    title_id = db.Column(db.Integer, db.ForeignKey(), nullable=False)
+
+    def __repr__(self):
+        return self
+
--- sanctum/routes.py
+++ sanctum/routes.py
@@ -5,3 +5,18 @@ from sanctum import app, db
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+
+@app.route("/books")
+def books():
+    return render_template("books.html")
+
+
+@app.route("/review")
+def review():
+    return render_template("review.html")
