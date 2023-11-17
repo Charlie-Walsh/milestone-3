0cf1d622562abdf667d69395045b50f9bff2eebe
Charlie-Walsh <charlie.walsh.141@gmail.com>
2023-11-17 20:11:11 +0000

    Edited long line in models.py

--- sanctum/models.py
+++ sanctum/models.py
@@ -5,7 +5,11 @@ class Title(db.Model):
     # schema for Title model
     id = db.Column(db.Integer, primary_key=True)
     book_title = db.Column(db.String(50), unique=True, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey("author.id"), nullable=False)
+    author_id = db.Column(
+        db.Integer,
+         db.ForeignKey("author.id"),
+          nullable=False
+    )
     reviews = db.relationship(
         "Review", backref="title", cascade="all, delete", lazy=True
     )
